58470731d2e48c7b8d9b99e8323f8fe757523784
Sean Overton <sean14o8.so@gmail.com>
2022-06-02 16:17:04 +1000

    Fixed rotating planet movement

--- Assets/Scripts/RotateCollider.cs
+++ Assets/Scripts/RotateCollider.cs
@@ -8,154 +8,46 @@ public class RotateCollider : MonoBehaviour
     GameObject targetPosition;
     [SerializeField]
     GameObject planetContainer;
-    [SerializeField]
-    private float movementSpeed = 10;
-    [SerializeField]
-    GameObject frontCollider;
-    [SerializeField]
-    GameObject backCollider;
-    [SerializeField]
-    GameObject leftCollider;
-    [SerializeField]
-    GameObject rightCollider;
-    [SerializeField]
-    public static string x_axis;
-    [SerializeField]
-    public static string z_axis;
-    [SerializeField]
-    float timeToRotate = 5f;
+
+    [SerializeField] private float _rollSpeed = 5;
+    private bool _isMoving;
 
     void Start(){
         targetPosition = GameObject.FindGameObjectWithTag("UpIndicator");
         planetContainer = GameObject.FindGameObjectWithTag("PlanetContainer");
-        frontCollider = GameObject.FindGameObjectWithTag("FrontCollider");
-        backCollider = GameObject.FindGameObjectWithTag("BackCollider");
-        leftCollider = GameObject.FindGameObjectWithTag("LeftCollider");
-        rightCollider = GameObject.FindGameObjectWithTag("RightCollider");
-        x_axis = "x";
-        z_axis = "z";
     }
-    void Update () {
-        // Draw a ray pointing at our target in
-        // Debug.DrawRay(transform.position, , Color.red);
-        // Debug.Log(gameObject.name+ " global roation: " + transform.rotation.eulerAngles);
-    }
+
     void OnTriggerEnter(Collider other)
     {   
         if (other.tag == "Player"){
-            // flip planet with this face as top
-            // Debug.Log("Collision with player detected on face " + gameObject.name);
-            
-            // Determine which direction to rotate towards
-            // Vector3 targetDirection = Vector3.up;
-            
             if(gameObject.tag == "LeftCollider"){
-                planetContainer.transform.RotateAround(targetPosition.transform.position, transform.forward, -90);
+                Assemble(Vector3.right);
             }
             if(gameObject.tag == "RightCollider"){
-                planetContainer.transform.RotateAround(targetPosition.transform.position, transform.forward, 90);
+                Assemble(Vector3.left);
             }
             if(gameObject.tag == "FrontCollider"){
-                planetContainer.transform.RotateAround(targetPosition.transform.position, transform.right, 90);
+                Assemble(Vector3.forward);
             }
             if(gameObject.tag == "BackCollider"){
-                planetContainer.transform.RotateAround(targetPosition.transform.position, transform.right, -90);
+                Assemble(Vector3.back);
             }
-
-            // float x_difference = transform.rotation.eulerAngles.x;
-            // float z_difference = transform.rotation.eulerAngles.z;
-
-            // if(x_difference >= 180){
-            //     x_difference = x_difference - 360;
-            // }else if(x_difference <= -180){
-            //     x_difference = x_difference + 360;
-            // }
-
-            // if(z_difference >= 180){
-            //     z_difference = z_difference - 360;
-            // }else if(z_difference <= -180){
-            //     z_difference = z_difference + 360;
-            // }
-
-            // StartCoroutine(RotateWorld(-x_difference, -z_difference));
-
-            // rotate itself and the opposite one
-            // GameObject collider1 = null;
-            // GameObject collider2 = null;
-
-            // if(gameObject.tag == "FrontCollider" || gameObject.tag == "BackCollider"){
-            //     collider1 = leftCollider;
-            //     collider2 = rightCollider;
-
-            //     collider1.transform.Rotate(
-            //         -x_difference, 
-            //         0, 
-            //         0, 
-            //         Space.World);
-
-            //     collider2.transform.Rotate(
-            //         -x_difference, 
-            //         0, 
-            //         0, 
-            //         Space.World);
-            // }
-
-            // if(gameObject.tag == "LeftCollider" || gameObject.tag == "RightCollider"){
-            //     collider1 = frontCollider;
-            //     collider2 = backCollider;
-                
-            //     collider1.transform.Rotate(
-            //         0, 
-            //         0, 
-            //         -z_difference, 
-            //         Space.World);
-
-            //     collider2.transform.Rotate(
-            //         0, 
-            //         0, 
-            //         -z_difference, 
-            //         Space.World);
-            // }
         }
     }
 
-    IEnumerator RotateWorld(float x_rotation, float z_rotation) {
-        float elapsedTime = 0.0f;
-        
-        float new_x_rotation = planetContainer.transform.rotation.eulerAngles.x+x_rotation;
-        float new_y_rotation = 0;
-        float new_z_rotation = planetContainer.transform.rotation.eulerAngles.z+z_rotation;
+    void Assemble(Vector3 dir) {
+        Vector3 anchor = planetContainer.transform.position;
+        Vector3 axis = Vector3.Cross(Vector3.up, dir);
+        StartCoroutine(Roll(anchor, axis));
+    }
 
-        Vector3 targetVector = new Vector3(
-                new_x_rotation,
-                new_y_rotation, 
-                new_z_rotation
-                );
-
-        Debug.Log("Target vector.x: "+targetVector.x);
-        Debug.Log("Target vector.y: "+targetVector.y);
-        Debug.Log("Target vector.z: "+targetVector.z);
-        
-        Quaternion targRot =  Quaternion.Euler(
-            targetVector
-            );
-        
-        Quaternion startingRotation = planetContainer.transform.rotation;
-
-        Debug.Log("Rotation: "+x_rotation+" "+z_rotation);
-        Debug.Log("Starting rotation: "+startingRotation.eulerAngles);
-        Debug.Log("Starting rotation: "+startingRotation);
-        Debug.Log("Target rotation: "+targRot.eulerAngles);
-        Debug.Log("Target rotation: "+targRot);
-
-        while (elapsedTime < timeToRotate) {
-            elapsedTime += Time.deltaTime; // <- move elapsedTime increment here
-            
-            planetContainer.transform.rotation = Quaternion.Slerp(startingRotation, targRot,  (elapsedTime / timeToRotate)  );
-
-            yield return new WaitForEndOfFrame ();
+    private IEnumerator Roll(Vector3 anchor, Vector3 axis) {
+        Debug.Log("Rolling");
+        _isMoving = true;
+        for (int i = 0; i < 90 / _rollSpeed; i++) {
+            planetContainer.transform.RotateAround(anchor, axis, _rollSpeed);
+            yield return new WaitForSeconds(0.01f);
         }
-        
-        StopCoroutine("RotateWorld");
+        _isMoving = false;
     }
 }
