27955155fc612c6c1d52ddbcfda8d41d0a138ca4
Sean Overton <sean14o8.so@gmail.com>
2022-06-07 09:10:36 +1000

    Updated asteroid spawner script to delete old asteroids

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class AsteroidSpawner : MonoBehaviour
 {
     [SerializeField]
-    public GameObject[] myObjects;
+    public GameObject[] objectsToSpawn;
+
+    public Queue<GameObject> spawnedObjects = new Queue<GameObject>();
 
     // Update is called once per frame
     void Update()
     {
         // some sort of wait method
         if(Random.Range(0, 100) == 5){
-            int randomIndex = Random.Range(0, myObjects.Length);
+            int randomIndex = Random.Range(0, objectsToSpawn.Length);
             Vector3 randomSpawnPosition = new Vector3(Random.Range(-20, 20), Random.Range(-20, 20), Random.Range(-20, 20));
-            GameObject ast = Instantiate(myObjects[randomIndex], randomSpawnPosition, Quaternion.identity);
+            GameObject ast = Instantiate(objectsToSpawn[randomIndex], randomSpawnPosition, Quaternion.identity);
+            
+            spawnedObjects.Enqueue(ast);
+
+            if(spawnedObjects.Count >= 5){
+                GameObject oldAst = spawnedObjects.Dequeue();
+                Destroy(oldAst);
+            }
 
             Rigidbody rb = ast.GetComponent<Rigidbody>();
             Vector3 randomForce = new Vector3(Random.Range(-10, 20), Random.Range(-10, 20), Random.Range(-10, 20));
