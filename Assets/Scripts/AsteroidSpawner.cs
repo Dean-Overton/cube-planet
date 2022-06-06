ef6a9133fb0ffd92b16c2603285092f50c21b940
Sean Overton <sean14o8.so@gmail.com>
2022-06-06 23:11:10 +1000

    Added asteroids + a house

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -11,10 +11,15 @@ public class AsteroidSpawner : MonoBehaviour
     void Update()
     {
         // some sort of wait method
+        if(Random.Range(0, 100) == 5){
+            int randomIndex = Random.Range(0, myObjects.Length);
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-20, 20), Random.Range(-20, 20), Random.Range(-20, 20));
+            GameObject ast = Instantiate(myObjects[randomIndex], randomSpawnPosition, Quaternion.identity);
 
-        int randomIndex = Random.Range(0, myObjects.Length);
-        Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 11), 5, Random.Range(-10, 11));
-    
-        Instantiate(myObjects[randomIndex], randomSpawnPosition, Quaternion.identity);
+            Rigidbody rb = ast.GetComponent<Rigidbody>();
+            Vector3 randomForce = new Vector3(Random.Range(-10, 20), Random.Range(-10, 20), Random.Range(-10, 20));
+
+            rb.AddForce(randomForce, ForceMode.Impulse);
+        }
     }
 }
